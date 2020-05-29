1a853e9763c3a79b1a260af732b2d0c35330b5d6
Márcio Augusto de Oliveira Figueiredo <maofigueiredo@gmail.com>
2020-05-29 08:42:51 -0300

    Basic implementation
    Basic and initial implementation of the "Scheduling Job" routine / utility.
    
    TO-DO:
    * Implementation of Test cases
    * Mass of input data
    * Logical implementation
    * Execution and testing
    
    Implementação básica e inicial da rotina / utilitário "Scheduling Job".
    
    TO-DO:
    * Implementação dos casos de Testes
    * Massa de dados de entrada
    * Implementação lógica
    * Execução e testes

---
+++ in/job-descriptions.js
@@ -0,0 +1,21 @@
+/* Input file: JOBS DESCRIPTION */
+/* Arquivo de entrada: DESCRIÇÃO DOS JOBS */
+
+/* MODEL / MODELO:
+
+[
+    id: 0, // number
+    description: 'description | descrição', // string
+    max_execution_date: '2099-12-31 12:00:00', // timestamp
+    estimated_time: '2 horas' // string [x hours |x horas]
+]
+
+*/
+
+const input_data = [
+    {
+        // TODO
+    }
+]
+
+export default input_data
---
+++ index.js
@@ -0,0 +1,33 @@
+/* Routine      : Scheduling Job
+   Version      : 0.0.1
+   Epic         : Job Utilities
+   Story        : As a Job Administrator, I need to organize a list of jobs in order to group them for execution within a time limit of 8 hours.
+   Squad        : Digital Innovation
+   Author       : Marcio A. O. Figueiredo <marciofweb@gmail.com>
+   Creation     : 2020-05-28
+   Last Change  : 2020-05-28
+   Instructions : See the README.md file
+*/
+
+/* IMPORTS */
+// TODO: Import INIT data
+
+// TODO: Create first test (TDD -> Fail)
+// TODO: Implement Grouping logic
+// TODO: Run tests (succes)
+// TODO: Run tests (fail)
+// TODO: Update instuctions and documentation
+
+const JobsGroupingForTimeLimit = (jobsToGrouping) => {
+
+    // TODO: Jobs Grouping logic
+
+    return jobsToGrouping
+
+}
+
+/* ROUTINE LOGIC */
+
+const routineResult = JobsGroupingForTimeLimit({ routine: 'run' })
+
+console.log(JSON.stringify(routineResult))
